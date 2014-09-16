3a162d2597642808e2cb2b7c4e1c6c748018996e
davidg <davidg@tid.es>
2014-09-16 12:09:05 +0200

    reconnect and control

--- termcaster.js
+++ termcaster.js
@@ -4,7 +4,8 @@ var getent = require('getent'),
     request = require('request'),
     server = process.env.TERMCASTER_SERVER || 'localhost:8080',
     pty = require('pty.js'),
-    WebSocket = require('ws');
+    WebSocket = require('ws'),
+    ws, wsControl;
 
 process.stdin.setRawMode(true);
 process.stdin.unpipe();
@@ -19,35 +20,65 @@ getent.passwd(process.env.LOGNAME, function(user) {
 
 function StartCasting(session_id, shell_command) {
     var url = 'ws://' + server + '/streamer/' + session_id,
-        ws = new WebSocket(url);
+        urlControl = 'ws://' + server + '/streamer_control/' + session_id;
+
+    var term = pty.spawn(shell_command, [], {
+        cols: process.stdout.columns,
+        rows: process.stdout.rows,
+        cwd: process.env.HOME,
+        env: process.env
+    });
+
+    term.on('data', function(data) {
+        try {
+            ws.send(data);
+        } catch(e) {
+            connect(url, function() {
+                ws.send(data);
+            });
+        }
+    });
+
+    process.stdout.on('resize', function() {
+        term.resize(process.stdout.columns, process.stdout.rows);
+        wsControl.send(JSON.stringify({
+            event: 'resize',
+            data: { 
+                columns: process.stdout.columns,
+                rows: process.stdout.rows
+            }
+        }));
+    });
+
+
+    term.on('close', function() {
+        term.socket.removeAllListeners();
+        process.stdin.unpipe();
+        process.stdin.pipe(process.stdout);
+        console.log('Termcasting finished');
+        process.exit();
+    })
+
+    term.socket.setEncoding = 'utf-8';
+    term.socket.pipe(process.stdout);
+    process.stdin.pipe(term.socket);
+
+    ws = connect(url);
+    wsControl = connect(urlControl);
+}
+
+function connect(url, cb) {
+    var ws = new WebSocket(url);
+
+    // Reconnect on close
+    ws.on('close', function() {
+        connect(url);
+    });
 
     ws.on('open', function() {
-        var term = pty.spawn(shell_command, [], {
-            cols: process.stdout.columns,
-            rows: process.stdout.rows,
-            cwd: process.env.HOME,
-            env: process.env
-        });
-
-        process.stdout.on('resize', function() {
-            term.resize(process.stdout.columns, process.stdout.rows);
-        });
-
-        term.on('data', function(data) {
-            ws.send(data);
-        });
-
-        term.on('close', function() {
-            term.socket.removeAllListeners();
-            process.stdin.unpipe();
-            process.stdin.pipe(process.stdout);
-            console.log('Termcasting finished');
-            process.exit();
-        })
-
-        term.socket.setEncoding = 'utf-8';
-        term.socket.pipe(process.stdout);
-        process.stdin.pipe(term.socket);
+        if (cb) { cb(); }
     });
+
+    return ws;
 }
 
